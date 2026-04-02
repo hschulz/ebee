5d6e86dc2454013d8bf96dbb30131cbcff274da1
Hauke Schulz <hauke27@gmail.com>
2026-04-02 10:12:35 +0200

    refactor: modernize tsconfig and move entry point to src/
    - Upgrade target to ES2022, module to NodeNext
    - Move index.ts from root into src/
    - Add .js extensions to all relative imports
    - Remove ~80 lines of commented-out template options

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { EventDispatcher } from "./EventDispatcher.js"
+import { EventDispatcherInterface } from "./EventDispatcherInterface.js"
+import { EventMap } from "./types.js"
+import { Registry } from "./Registry.js"
+import { Subscribers } from "./Subscribers.js"
+
+export {
+    EventDispatcher,
+    EventDispatcherInterface,
+    EventMap,
+    Registry,
+    Subscribers
+}
