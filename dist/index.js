d62f74381aaa180acefc34ef5a3ca4a8b39bbffc
Hauke Schulz <hauke27@gmail.com>
2026-04-02 10:12:26 +0200

    chore: add dist/ to .gitignore and remove tracked build artifacts

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EventDispatcher = void 0;
-const EventDispatcher_1 = require("./src/EventDispatcher");
-Object.defineProperty(exports, "EventDispatcher", { enumerable: true, get: function () { return EventDispatcher_1.EventDispatcher; } });
-//# sourceMappingURL=index.js.map
--- dist/src/Subscribers.d.ts
+++
@@ -1,8 +0,0 @@
-import { EventMap } from "./types";
-/**
- * Subscribers are a collection of callbacks for an event.
- */
-export type Subscribers<T extends EventMap> = {
-    [K in keyof T]?: Set<T[K]>;
-};
-//# sourceMappingURL=Subscribers.d.ts.map
