1e36b577942a2b4a35ba9160f15459dd2b60ed41
Hauke Schulz <hauke27@gmail.com>
2026-04-02 10:12:52 +0200

    feat: migrate testing from Mocha/Chai/nyc to Vitest
    - Replace mocha, chai, nyc, ts-node with vitest and @vitest/coverage-v8
    - Rewrite all tests to use vitest API (describe/it/expect)
    - Add exports, files, engines fields to package.json
    - Bump version to 0.3.0

--- test/Ebee.test.ts
+++ test/Ebee.test.ts
@@ -1,183 +1,163 @@
-import { EventDispatcher } from ".."
-import { expect } from "chai"
+import { describe, it, expect, beforeEach, afterEach } from "vitest"
+import { EventDispatcher } from "../src/index.js"
 
 type TestEventsMap = {
-    "mocha.test.register": () => void
-    "mocha.test.unregister": () => void
-    "mocha.test.multi": () => void
-    "mocha.test.before.reset": () => void
-    "mocha.test.after.reset": () => void
-    "mocha.test.with.arg": (arg: string) => void
-    "mocha.test.with.args": (arg1: string, arg2: string) => void
+    "test.register": () => void
+    "test.unregister": () => void
+    "test.multi": () => void
+    "test.before.reset": () => void
+    "test.after.reset": () => void
+    "test.with.arg": (arg: string) => void
+    "test.with.args": (arg1: string, arg2: string) => void
 }
 
-describe("EBEE", async (): Promise<void> => {
+describe("EBEE", () => {
 
-    let dispatcher: EventDispatcher<TestEventsMap> | undefined = undefined
+    let dispatcher: EventDispatcher<TestEventsMap>
 
     beforeEach(() => {
         dispatcher = EventDispatcher.get<TestEventsMap>()
     })
 
     afterEach(() => {
-        dispatcher?.reset()
+        dispatcher.reset()
     })
 
-    it("should register callbacks to the event bus", (done: Mocha.Done) => {
+    it("should register callbacks to the event bus", async () => {
+        const eventName = "test.register"
+        let called = false
 
-        const eventName = "mocha.test.register"
+        dispatcher.register(eventName, () => { called = true })
+        await dispatcher.dispatch(eventName)
 
-        dispatcher?.register(eventName, done)
-
-        dispatcher?.dispatch(eventName)
+        expect(called).toBe(true)
     })
 
-    it("should unregister callbacks from the event bus", (done: Mocha.Done) => {
+    it("should unregister callbacks from the event bus", async () => {
+        const eventName = "test.unregister"
+        let called = false
 
-        const eventName = "mocha.test.unregister"
-
-        const timeoutId = setTimeout(() => {
-            done()
-        }, 23)
-
-        const reg = dispatcher?.register(eventName, () => {
-            clearTimeout(timeoutId)
-            done(new Error("callback was not removed"))
+        const reg = dispatcher.register(eventName, () => {
+            called = true
         })
 
-        reg?.unregister()
+        await reg.unregister()
+        await dispatcher.dispatch(eventName)
 
-        dispatcher?.dispatch(eventName)
+        expect(called).toBe(false)
     })
 
-    it("should unregister multiple callbacks from the event bus", (done: Mocha.Done) => {
+    it("should unregister multiple callbacks from the event bus", async () => {
+        const eventName = "test.multi"
+        let called1 = false
+        let called2 = false
+        let called3 = false
 
-        const eventName = "mocha.test.multi"
+        const reg1 = dispatcher.register(eventName, () => { called1 = true })
+        const reg2 = dispatcher.register(eventName, () => { called2 = true })
+        dispatcher.register(eventName, () => { called3 = true })
 
-        const reg1 = dispatcher?.register(eventName, () => {
-            done(new Error("callback 1 was not removed"))
-        })
+        await reg1.unregister()
+        await reg2.unregister()
+        await dispatcher.dispatch(eventName)
 
-        const reg2 = dispatcher?.register(eventName, () => {
-            done(new Error("callback 2 was not removed"))
-        })
-
-        dispatcher?.register(eventName, done)
-
-        reg1?.unregister()
-        reg2?.unregister()
-
-        dispatcher?.dispatch(eventName)
+        expect(called1).toBe(false)
+        expect(called2).toBe(false)
+        expect(called3).toBe(true)
     })
 
-    it("should create a new default instance", async (): Promise<void> => {
-        expect(dispatcher).to.be.instanceOf(EventDispatcher)
+    it("should create a new default instance", () => {
+        expect(dispatcher).toBeInstanceOf(EventDispatcher)
 
         const dispatcher2 = EventDispatcher.get<TestEventsMap>("default")
-
-        expect(dispatcher2).to.be.instanceOf(EventDispatcher)
-
-        expect(dispatcher).to.equal(dispatcher2)
+        expect(dispatcher2).toBeInstanceOf(EventDispatcher)
+        expect(dispatcher).toBe(dispatcher2)
     })
 
-    it("should create new named instances", async (): Promise<void> => {
+    it("should create new named instances", () => {
         const newDispatcher = EventDispatcher.get<TestEventsMap>("test")
 
-        expect(newDispatcher).to.be.instanceOf(EventDispatcher)
-
-        expect(newDispatcher).to.not.equal(dispatcher)
+        expect(newDispatcher).toBeInstanceOf(EventDispatcher)
+        expect(newDispatcher).not.toBe(dispatcher)
     })
 
-    it("should be able to reset the instance", (done: Mocha.Done) => {
+    it("should be able to reset the instance", async () => {
+        let beforeCalled = false
+        let afterCalled = false
 
-        dispatcher?.register("mocha.test.before.reset", () => {
-            done(new Error("Failed to reset"))
-        })
+        dispatcher.register("test.before.reset", () => { beforeCalled = true })
+        dispatcher.reset()
+        await dispatcher.dispatch("test.before.reset")
 
-        dispatcher?.reset()
+        dispatcher.register("test.after.reset", () => { afterCalled = true })
+        await dispatcher.dispatch("test.after.reset")
 
-        dispatcher?.dispatch("mocha.test.before.reset")
-
-        dispatcher?.register("mocha.test.after.reset", () => {
-            done()
-        })
-
-        dispatcher?.dispatch("mocha.test.after.reset")
+        expect(beforeCalled).toBe(false)
+        expect(afterCalled).toBe(true)
     })
 
-    it("should dispatch an event with arguments", (done: Mocha.Done) => {
+    it("should dispatch an event with arguments", async () => {
+        const eventName = "test.with.arg"
+        let receivedArg = ""
 
-        const eventName = "mocha.test.with.arg"
-
-        dispatcher?.register(eventName, (arg: string) => {
-            expect(arg).to.equal("test")
-            done()
+        dispatcher.register(eventName, (arg: string) => {
+            receivedArg = arg
         })
 
-        dispatcher?.dispatch(eventName, "test")
+        await dispatcher.dispatch(eventName, "test")
+
+        expect(receivedArg).toBe("test")
     })
 
-    it("should dispatch an event with multiple arguments", (done: Mocha.Done) => {
+    it("should dispatch an event with multiple arguments", async () => {
+        const eventName = "test.with.args"
+        let receivedArg1 = ""
+        let receivedArg2 = ""
 
-        const eventName = "mocha.test.with.args"
-
-        dispatcher?.register(eventName, (arg1: string, arg2: string) => {
-            expect(arg1).to.equal("test")
-            expect(arg2).to.equal("test")
-            done()
+        dispatcher.register(eventName, (arg1: string, arg2: string) => {
+            receivedArg1 = arg1
+            receivedArg2 = arg2
         })
 
-        dispatcher?.dispatch(eventName, "test", "test")
+        await dispatcher.dispatch(eventName, "test", "test")
+
+        expect(receivedArg1).toBe("test")
+        expect(receivedArg2).toBe("test")
     })
 
-    it("should dispatch events only to the correct subscribers", (done: Mocha.Done) => {
+    it("should be possible to run a registry unregister even if the event is no longer registered", async () => {
+        const eventName = "test.register"
+        let called = false
 
-        const eventName = "mocha.test.with.arg"
-
-        dispatcher?.register(eventName, (arg: string) => {
-            expect(arg).to.equal("test")
-            done()
+        const reg = dispatcher.register(eventName, () => {
+            called = true
         })
 
-        dispatcher?.dispatch("mocha.test.with.arg", "wrong")
-        dispatcher?.dispatch(eventName, "test")
+        dispatcher.reset()
+        await reg.unregister()
+        await dispatcher.dispatch(eventName)
+
+        expect(called).toBe(false)
     })
 
-    it("should be possible to run a registry unregister even if the event is no longer registered", (done: Mocha.Done) => {
+    it("should be possible to run a registry unregister multiple times", async () => {
+        const eventName = "test.register"
+        let called = false
 
-        const eventName = "mocha.test.register"
-
-        const reg = dispatcher?.register(eventName, () => {
-            done(new Error("callback was not removed"))
+        const reg = dispatcher.register(eventName, () => {
+            called = true
         })
 
-        dispatcher?.reset()
+        dispatcher.register(eventName, () => {})
 
-        reg?.unregister()
+        dispatcher.reset()
 
-        dispatcher?.dispatch(eventName)
+        await reg.unregister()
+        await reg.unregister()
+        await reg.unregister()
 
-        done()
-    })
+        await dispatcher.dispatch(eventName)
 
-    it("should be possible to run a registry unregister multiple times", (done: Mocha.Done) => {
-
-        const eventName = "mocha.test.register"
-
-        const reg = dispatcher?.register(eventName, () => {
-            done(new Error("callback was not removed"))
-        })
-
-        dispatcher?.register(eventName, () => {})
-
-        dispatcher?.reset()
-
-        reg?.unregister()
-        reg?.unregister()
-        reg?.unregister()
-
-        dispatcher?.dispatch(eventName)
-
-        done()
+        expect(called).toBe(false)
     })
 })
